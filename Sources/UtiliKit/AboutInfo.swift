d00100f4971e0dcd5ee762b306cb461038d35911
Stephen Bensley <76857027+stephenbensley@users.noreply.github.com>
2024-09-04 10:30:19 -0700

    Add AboutInfo and AboutView

---
+++ Sources/UtiliKit/AboutInfo.swift
@@ -0,0 +1,69 @@
+//
+// Copyright 2024 Stephen E. Bensley
+//
+// This file is licensed under the MIT License. You may obtain a copy of the
+// license at https://github.com/stephenbensley/UtiliKit/blob/main/LICENSE.
+//
+
+import Foundation
+
+// Exposes app-specific properties for the app's about page or similar scenarios.
+public protocol AboutInfo {
+    // ID used to identify the app on the AppStore
+    var appStoreId: Int { get }
+   // Copyright notice
+    var copyright: String { get }
+    // Brief description suitable for a sub-title.
+    var description: String { get }
+    // GitHub account that hosts the repo
+    var gitHubAccount: String { get }
+    // GitHub repo -- just the repo name, not the full URL
+    var gitHubRepo: String { get }
+}
+
+// Additional info that can be derived automatically.
+public extension AboutInfo {
+    public var name: String {
+        Bundle.main.infoDictionary?["CFBundleName"] as? String ?? ""
+    }
+
+    public var version: String {
+        Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "0.0.0"
+    }
+
+    public var contact: URL {
+        .init(string: "https://github.com/\(gitHubAccount)")!
+    }
+
+    // Assumes the license file is called LICENSE and is located at the root of the repo.
+    public var license: URL {
+        .init(string: "https://github.com/\(gitHubAccount)/\(gitHubRepo)/blob/main/LICENSE")!
+    }
+    
+    // Assumes the privacy policy is called privacy.html and has been published to the root of
+    // github.io.
+    public var privacyPolicy: URL {
+        .init(string: "https://\(gitHubAccount).github.io/\(gitHubRepo)/privacy.html")!
+    }
+    
+    public var share: URL {
+        .init(string: "https://apps.apple.com/us/app/id\(appStoreId)")!
+    }
+    
+    public var sourceCode: URL {
+        .init(string: "https://github.com/\(gitHubAccount)/\(gitHubRepo)")!
+    }
+    
+    public var writeReview: URL {
+        .init(string: "https://apps.apple.com/us/app/id\(appStoreId)?action=write-review")!
+    }
+}
+
+// Useful for previews and testing.
+public struct MockAboutInfo: AboutInfo {
+    public var appStoreId: Int = 1631745251
+    public var copyright: String = "© 2024 Stephen E. Bensley"
+    public var description: String = "This is a mock app for testing purposes."
+    public var gitHubAccount: String = "stephenbensley"
+    public var gitHubRepo: String = "UrCoach"
+ }
---
+++ Sources/UtiliKit/AboutView.swift
@@ -0,0 +1,80 @@
+//
+// Copyright 2024 Stephen E. Bensley
+//
+// This file is licensed under the MIT License. You may obtain a copy of the
+// license at https://github.com/stephenbensley/UtiliKit/blob/main/LICENSE.
+//
+
+import SwiftUI
+
+// Displays a generic About page.
+public struct AboutView: View {
+    @Environment(\.dismiss) private var dismiss
+    private var info: AboutInfo
+
+    public init(info: some AboutInfo) {
+        self.info = info
+    }
+
+    public var body: some View {
+        NavigationStack {
+            Form {
+                HStack(alignment: .center) {
+                    Image(uiImage: UIImage(named: "AppIcon") ?? UIImage())
+                        .resizable()
+                        .frame(width: 64, height: 64)
+                        .cornerRadius(10)
+                        .padding(.trailing, 5)
+                    VStack(alignment: .leading) {
+                        Text(info.name)
+                            .font(.title)
+                        Text("Version \(info.version)\n\(info.copyright)")
+                            .font(.footnote)
+                    }
+                }
+                
+                Section {
+                    Link(destination: info.privacyPolicy) {
+                        Label("Read the privacy policy", systemImage: "hand.raised")
+                    }
+                } header: {
+                    Text("This app does not collect or share any personal information.")
+                }
+                .textCase(nil)
+                
+                Section {
+                    Link(destination: info.license) {
+                        Label("Read the license", systemImage: "doc.plaintext")
+                    }
+                    Link(destination: info.sourceCode) {
+                        Label("Download the source code", systemImage: "icloud.and.arrow.down")
+                    }
+                } header: {
+                    Text("""
+                    The source code for this app has been released under the MIT License and is \
+                    hosted on GitHub.
+                    """)
+                }
+                .textCase(nil)
+                
+                Section {
+                    Link(destination: info.writeReview) {
+                        Label("Rate this app", systemImage: "star")
+                    }
+                    ShareLink(item: info.share) {
+                        Label("Share this app", systemImage:  "square.and.arrow.up")
+                    }
+                    Link(destination: info.contact) {
+                        Label("Contact the developer", systemImage: "mail")
+                    }
+                 }
+            }
+            .navigationTitle("About")
+            .toolbar { Button("Done") { dismiss() } }
+          }
+    }
+}
+
+#Preview {
+    AboutView(info: MockAboutInfo())
+}
