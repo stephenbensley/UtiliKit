4eebd5dfedefad93bc40152a4522b0c269be2fce
Stephen Bensley <76857027+stephenbensley@users.noreply.github.com>
2024-09-08 12:28:56 -0700

    Update header text

--- Sources/CheckersKit/Palette.swift
+++ Sources/CheckersKit/Palette.swift
@@ -2,7 +2,7 @@
 // Copyright 2024 Stephen E. Bensley
 //
 // This file is licensed under the MIT License. You may obtain a copy of the
-// license at https://github.com/stephenbensley/CheckersKit/blob/main/LICENSE.
+// license at https://github.com/stephenbensley/UtiliKit/blob/main/LICENSE.
 //
 
 import SwiftUI
